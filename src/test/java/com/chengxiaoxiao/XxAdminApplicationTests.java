66ae11196ac9e3542fc7884bfa4f661088ab0db9
Cheng xiaoxiao <iquanzhan@163.com>
2020-01-05 17:51:50 +0800

    初始化项目

---
+++ src/test/java/com/chengxiaoxiao/XxAdminApplicationTests.java
@@ -0,0 +1,16 @@
+package com.chengxiaoxiao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class XxAdminApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
