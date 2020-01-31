93ef4f840ac4b5798efcd0466eb762a1563a8728
Cheng xiaoxiao <iquanzhan@163.com>
2020-01-31 22:28:59 +0800

    [Web]添加通用响应对象及通用异常

--- admin-web/src/main/java/com/chengxiaoxiao/web/exception/GlobleExceptionHandler.java
+++ admin-web/src/main/java/com/chengxiaoxiao/web/exception/GlobleExceptionHandler.java
@@ -1,11 +1,10 @@
 package com.chengxiaoxiao.web.exception;
 
-import com.chengxiaoxiao.model.common.dtos.result.CodeMsg;
-import com.chengxiaoxiao.model.common.dtos.result.Result;
+import com.chengxiaoxiao.model.common.dtos.CodeMsg;
+import com.chengxiaoxiao.model.common.dtos.Result;
 import org.springframework.validation.BindException;
-import org.springframework.validation.BindingResult;
 import org.springframework.validation.ObjectError;
-import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.HttpRequestHandler;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseBody;
@@ -21,7 +20,6 @@ public class GlobleExceptionHandler {
     @ExceptionHandler(value = Exception.class)
     public Result<String> exceptionHandler(HttpServletRequest request, Exception e) {
         e.printStackTrace();
-
         if (e instanceof BindException) {
             BindException ex = (BindException) e;
             List<ObjectError> errors = ex.getAllErrors();
@@ -29,17 +27,11 @@ public class GlobleExceptionHandler {
             String msg = error.getDefaultMessage();
 
             return Result.error(CodeMsg.BIND_ERROR.fillArgs(msg));
-        }
-        else if(e instanceof MethodArgumentNotValidException){
-            MethodArgumentNotValidException ex = (MethodArgumentNotValidException) e;
-
-            return Result.error(CodeMsg.BIND_ERROR.fillArgs(ex.getBindingResult().getAllErrors().get(0).getDefaultMessage()));
-        }
-        else if (e instanceof GlobleException) {
+        } else if (e instanceof GlobleException) {
             GlobleException ex = (GlobleException) e;
             return Result.error(ex.getCm());
         } else {
-            return Result.error(CodeMsg.OTHER_ERROR);
+            return Result.error(CodeMsg.ERROR);
         }
     }
 }
